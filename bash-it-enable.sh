3a17c30661742b15d4ddf62be86f23f02fe6861d
Nils Winkler <nils@nilswinkler.com>
2013-09-09 10:30:15 +0200

    Added script to enable my favorite bash-it settings.

---
+++ bash-it-enable.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Aliases
+bash-it enable alias general
+bash-it enable alias git
+bash-it enable alias homebrew
+bash-it enable alias maven
+bash-it enable alias nwinkler
+bash-it enable alias osx
+bash-it enable alias vim
+
+# Completions
+bash-it enable completion bash-it
+bash-it enable completion brew
+bash-it enable completion defaults
+bash-it enable completion git
+bash-it enable completion git_flow
+bash-it enable completion homesick
+bash-it enable completion maven
+bash-it enable completion ssh
+
+# Plugins
+bash-it enable plugin audio
+bash-it enable plugin base
+bash-it enable plugin battery
+bash-it enable plugin browser
+bash-it enable plugin dirs
+bash-it enable plugin extract
+bash-it enable plugin fasd
+bash-it enable plugin git
+bash-it enable plugin java
+bash-it enable plugin javascript
+bash-it enable plugin maven
+bash-it enable plugin node
+bash-it enable plugin osx
+bash-it enable plugin proxy
+bash-it enable plugin python
+bash-it enable plugin ssh
+bash-it enable plugin subversion
