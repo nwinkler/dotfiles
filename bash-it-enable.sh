0f7f84594a8494a647ae7b91537700458cb5bc31
Nils Winkler <nils@nilswinkler.com>
2013-11-08 09:49:34 +0100

    Added gitsvn aliases

--- bash-it-enable.sh
+++ bash-it-enable.sh
@@ -5,6 +5,7 @@ source ~/.bash_it/bash_it.sh
 # Aliases
 bash-it enable alias general
 bash-it enable alias git
+bash-it enable alias gitsvn
 bash-it enable alias homebrew
 bash-it enable alias maven
 bash-it enable alias nwinkler
