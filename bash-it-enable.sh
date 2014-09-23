0c969b29cf56eb4c3f60f33f1fe1fb605772e022
Nils Winkler <nils@nilswinkler.com>
2014-09-23 14:57:24 +0200

    Removed fasd plugin.

--- bash-it-enable.sh
+++ bash-it-enable.sh
@@ -29,7 +29,6 @@ bash-it enable plugin battery
 bash-it enable plugin browser
 bash-it enable plugin dirs
 bash-it enable plugin extract
-bash-it enable plugin fasd
 bash-it enable plugin git
 bash-it enable plugin java
 bash-it enable plugin javascript
