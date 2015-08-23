5a8ac60782ce40fc3ea4ac17385a00539f128877
Nils Winkler <nils@nilswinkler.com>
2015-08-23 14:36:42 +0200

    Modified user check

--- install_homebrew.sh
+++ install_homebrew.sh
@@ -2,7 +2,7 @@
 
 sudo mkdir -p /usr/local
 
-sudo chown -R nils.winkler:staff /usr/local
+sudo chown -R $USER:staff /usr/local
 
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
