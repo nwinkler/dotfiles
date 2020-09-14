24a8b921eecfcb9621a6dca61bd1a9a7723fa893
Nils Winkler <nils@nilswinkler.com>
2020-09-14 10:50:04 +0200

    Cleaned up Docker usage and a couple of others

--- install_dev.sh
+++ install_dev.sh
@@ -13,12 +13,6 @@ brew install shellcheck
 
 brew install diff-so-fancy
 
-brew install docker
-
-brew install docker-machine
-
-brew install docker-compose
-
 # Inspect Docker Image layers
 # https://github.com/wagoodman/dive
 brew tap wagoodman/dive
@@ -30,10 +24,6 @@ brew install jenv
 
 brew install maven
 
-brew install mongodb
-
-brew install mysql
-
 export HOMEBREW_CASK_OPTS=--appdir=/Applications
 
 sudo chown root:staff /Applications
@@ -42,20 +32,13 @@ brew cask install diffmerge
 rm /usr/local/bin/diffmerge
 ln -s /Applications/DiffMerge.app/Contents/Resources/diffmerge.sh /usr/local/bin/diffmerge
 
+brew cask install docker
+
 brew cask install licecap
 
 brew cask install mac-linux-usb-loader
 
-brew cask install mou
-
-brew cask install mysqlworkbench
-
 brew cask install netspot
-
-brew cask install p4merge
-
-curl http://pempek.net/files/git-p4merge/mac/p4merge -o ~/bin/p4merge
-chmod +x ~/bin/p4merge
 
 brew cask install virtualbox
 brew cask install vagrant
